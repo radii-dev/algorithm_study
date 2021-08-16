3be818962c97711b50f8da677e34e8a686ce135b
Seongwook Kang <gigaslender2@gachon.ac.kr>
2021-08-16 20:22:36 +0900

    Add solution to exercise problem in basic-1

--- BaekJoon/lecture/algorithm-basic-1/src/10828.cpp
+++ BaekJoon/lecture/algorithm-basic-1/src/10828.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
-#include <vector>
-
+#define fio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 using namespace std;
+typedef long long int ll;
+
+#include <stack>
+#include <string>
+
+int N;
+
+void readInput() {
+    cin >> N;
+}
+
+void solve() {
+    readInput();
+    string cmd;
+    stack<int> ans;
+    for (int i = 0; i < N; i++) {
+        cin >> cmd;
+        if (cmd == "push") {
+            int num;
+            cin >> num;
+            ans.push(num);
+        }
+        else if (cmd == "pop") {
+            if (ans.empty()) cout << -1 << "\n";
+            else {
+                cout << ans.top() << "\n";
+                ans.pop();
+            }
+        }
+        else if (cmd == "size") 
+            cout << ans.size() << "\n";
+        else if (cmd == "empty") {
+            if (ans.empty()) cout << 1 << "\n";
+            else cout << 0 << "\n";
+        } 
+        else if (cmd == "top") {
+            if (ans.empty()) cout << -1 << "\n";
+            else cout << ans.top() << "\n";
+        } 
+    }
+}
 
 int main (int argc, char* argv[]) {
-    cout << "test pro" << "\n";
+    fio;
+    freopen("10828_tc2.txt", "r", stdin);
 
+    solve();
+    
     return 0;
 }
 
---
+++ BaekJoon/lecture/algorithm-basic-1/src/template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define fio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+typedef long long int ll;
+
+
+void readInput() {
+
+}
+
+void solve() {
+    readInput();
+
+}
+
+int main (int argc, char* argv[]) {
+    fio;
+    freopen("tc.txt", "r", stdin);
+
+    solve();
+
+    return 0;
+}
+
