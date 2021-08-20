7fa52e86d99be7d67c9c9677c503d1e7c29995ca
Seongwook Kang <gigaslender2@gachon.ac.kr>
2021-08-20 22:57:14 +0900

    Add soultion of exercise problem no 1406

---
+++ BaekJoon/lecture/algorithm-basic-1/src/1406.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define fio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+typedef long long int ll;
+
+#include <stack>
+#include <string>
+
+int N, M;
+string str;
+char cmd[][2];
+
+void readInput() {
+    getline(cin, str);
+    N = str.length();
+    cin >> M;
+    cin.ignore();
+    for (int i = 0; i < M; ++i) 
+}
+
+void solve() {
+    readInput();
+    
+}
+
+int main (int argc, char* argv[]) {
+    fio;
+    freopen("input1.txt", "r", stdin);
+
+    solve();
+
+    return 0;
+}
+
