ea029d38f982775e8cfc66b3fafcafed42ef9fbb
Seongwook Kang <gigaslender2@gachon.ac.kr>
2021-08-21 12:05:11 +0900

    Add solution of exercise problem no 1406

--- BaekJoon/lecture/algorithm-basic-1/src/1406.cpp
+++ BaekJoon/lecture/algorithm-basic-1/src/1406.cpp
@@ -8,24 +8,47 @@ typedef long long int ll;
 
 int N, M;
 string str;
-char cmd[][2];
+stack<char> cursor_left, cursor_right;
 
 void readInput() {
     getline(cin, str);
-    N = str.length();
+    for (string::iterator iter = str.begin(); iter != str.end(); ++iter) 
+        cursor_left.push(*iter);
     cin >> M;
-    cin.ignore();
-    for (int i = 0; i < M; ++i) 
+    for (int i = 0; i < M; ++i) {
+        char cmd;
+        cin >> cmd;
+        if (cmd == 'L' && !cursor_left.empty()) {
+            cursor_right.push(cursor_left.top());
+            cursor_left.pop();
+        } else if (cmd == 'D' && !cursor_right.empty()) {
+            cursor_left.push(cursor_right.top());
+            cursor_right.pop();
+        } else if (cmd == 'B' && !cursor_left.empty()) {
+            cursor_left.pop();
+        } else if (cmd == 'P') {
+            cin >> cmd;
+            cursor_left.push(cmd);
+        }
+    }
 }
 
 void solve() {
     readInput();
-    
+    while (!cursor_left.empty()) {
+        cursor_right.push(cursor_left.top());
+        cursor_left.pop();
+    }
+    while (!cursor_right.empty()) {
+        cout << cursor_right.top();
+        cursor_right.pop();
+    }
+    cout << "\n";
 }
 
 int main (int argc, char* argv[]) {
     fio;
-    freopen("input1.txt", "r", stdin);
+    freopen("./bin/input3.txt", "r", stdin);
 
     solve();
 
